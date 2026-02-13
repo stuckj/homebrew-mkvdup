f52fe3ecb3455841541c8c7e20fc01e01deaadab
stuckj <stuckj@users.noreply.github.com>
2026-02-13 21:05:34 +0000

    Update formula for v0.9.2-canary.2

--- Formula/mkvdup-canary.rb
+++ Formula/mkvdup-canary.rb
@@ -11,27 +11,27 @@ class MkvdupCanary < Formula
   desc "Storage deduplication tool for MKV files (canary/pre-release)"
   homepage "https://github.com/stuckj/mkvdup"
   license "MIT"
-  version "0.9.2-canary.1"
+  version "0.9.2-canary.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.1/mkvdup-canary_darwin_arm64.tar.gz"
-      sha256 "e9f12058f6fc36551a50763be0c31ee635c307aa39953ae6ba19f9f625f14200"
+      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.2/mkvdup-canary_darwin_arm64.tar.gz"
+      sha256 "f4d967cb57c2d9bf61273fa6e0a665b374dde1734c4f26104bfdb4a91b5a89d5"
     end
     on_intel do
-      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.1/mkvdup-canary_darwin_amd64.tar.gz"
-      sha256 "4c8e88e14bd2100aeb83517eb6c054db67b8c7eb0c7861c75efe2ff9a536e8c2"
+      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.2/mkvdup-canary_darwin_amd64.tar.gz"
+      sha256 "5f087c0cd1216b8348b7993e50ac78db566720ac69c3768171239e9385a21a1c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.1/mkvdup-canary_linux_arm64.tar.gz"
-      sha256 "d339022cdce9ee1cfa959d7948753fa36f97d18ecd1c1ab3237fe9d7b8042a8e"
+      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.2/mkvdup-canary_linux_arm64.tar.gz"
+      sha256 "f5ee99579292013063142e08f50a71db436502e2d4a7e833e49ea268368dfb80"
     end
     on_intel do
-      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.1/mkvdup-canary_linux_amd64.tar.gz"
-      sha256 "3b86493e29c736481910d4e1f32678c8307f2b4c43eafde14414c05456043824"
+      url "https://github.com/stuckj/mkvdup/releases/download/v0.9.2-canary.2/mkvdup-canary_linux_amd64.tar.gz"
+      sha256 "beb1c870536167a649cc573f60174b654aff691f86aac3af16f7be64637c9823"
     end
   end
 
